56c7897f0a4b3cb6d85a3a58b8e5373c22215cf2
Prabanjan <itsprabanjan@gmail.com>
2024-07-02 03:19:13 +0530

    This is the example of MySQLi database connection method

--- feedback.php
+++ feedback.php
@@ -1,7 +1,7 @@
 <?php include 'header.php' ?>
 
 <?php 
-  $sql = "SELECT * FROM feedbacks";
+  $sql = "SELECT * FROM feedbacks ORDER BY id DESC";
   $result = mysqli_query($conn, $sql);
   $feedbacks = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?> 
@@ -17,7 +17,7 @@
     <div class="card-body text-center">
       <?php echo $feedback['body'] ?>
       <div class="text-secondary mt-2" style="font-size: 12px;">
-        Posted by <b><?php echo $feedback['name'] ?></b> on 
+        Posted by <b><?php echo ucfirst($feedback['name']) ?></b> on 
         <?php echo $feedback['date_time'] ?>
       </div>
     </div>
